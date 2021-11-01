0dd43a43d2649bafc17a838edba3742834cc8d5e
Prasanta Barman <prasantabarman06@gmail.com>
2021-11-02 00:42:46 +0530

    initial commit

---
+++ entry/src/main/java/com/sithagi/kitkatemoji/MyApplication.java
@@ -0,0 +1,10 @@
+package com.sithagi.kitkatemoji;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
